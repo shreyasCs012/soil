21ce4a878db67850d35fb2eb10bb48512e90f75d
Sharadhi18 <sharadhikv@gmail.com>
2026-05-21 22:23:43 +0530

    Update sketch_apr11a.ino

--- sketch_apr11a.ino
+++ sketch_apr11a.ino
@@ -16,6 +16,24 @@ int relayPin = 7;
 // Soil threshold
 int dryThreshold = 700;
 
+void setup() {#include <SoftwareSerial.h>
+#include <DHT.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// Bluetooth
+SoftwareSerial BT(10, 11);
+
+// Pins
+int soilPin = A0;
+int relayPin = 7;
+
+// Soil threshold
+int dryThreshold = 700;
+
 void setup() {
 
   Serial.begin(9600);
@@ -45,19 +63,100 @@ void loop() {
   // DISPLAY DATA
   // =========================
 
-  Serial.print("Soil:");
+  Serial.print("Soil moisture:");
   Serial.print(soilValue);
 
-  Serial.print(" Temp:");
+  Serial.print(" Temperature:");
   Serial.print(temp);
 
   Serial.print(" Humidity:");
   Serial.println(hum);
 
-  BT.print("Soil:");
+  BT.print("Soil moisture:");
   BT.print(soilValue);
 
-  BT.print(" Temp:");
+  BT.print(" Temperature:");
+  BT.print(temp);
+
+  BT.print(" Humidity:");
+  BT.println(hum);
+
+  // =========================
+  // SMART IRRIGATION
+  // =========================
+
+  while (soilValue > dryThreshold) {
+
+    Serial.println("Soil Dry -> Pump ON");
+
+    // Pump ON
+    digitalWrite(relayPin, LOW);
+
+    // Water for 5 seconds
+    delay(5000);
+
+    // Pump OFF
+    digitalWrite(relayPin, HIGH);
+
+    Serial.println("Pump OFF");
+
+    // Wait 10 seconds
+    Serial.println("Waiting 10 seconds...");
+
+    delay(10000);
+
+    // Check soil again
+    soilValue = analogRead(soilPin);
+
+    Serial.print("New Soil Value: ");
+    Serial.println(soilValue);
+  }
+
+  Serial.println("Soil Wet -> No Water Needed");
+
+  delay(3000);
+}
+
+  Serial.begin(9600);
+
+  BT.begin(9600);
+
+  dht.begin();
+
+  pinMode(relayPin, OUTPUT);
+
+  // Pump OFF initially
+  digitalWrite(relayPin, HIGH);
+
+  Serial.println("Smart Agro System Started");
+}
+
+void loop() {
+
+  // Read sensors
+  int soilValue = analogRead(soilPin);
+
+  float temp = dht.readTemperature();
+
+  float hum = dht.readHumidity();
+
+  // =========================
+  // DISPLAY DATA
+  // =========================
+
+  Serial.print("Soil moisture:");
+  Serial.print(soilValue);
+
+  Serial.print(" Temperature:");
+  Serial.print(temp);
+
+  Serial.print(" Humidity:");
+  Serial.println(hum);
+
+  BT.print("Soil moisture:");
+  BT.print(soilValue);
+
+  BT.print(" Temperature:");
   BT.print(temp);
 
   BT.print(" Humidity:");
